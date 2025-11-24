258f916c23cecfc3eaba971daae4bd5d9d180295
Fossilest2 <rui@Louis.local>
2025-11-24 15:54:04 +0900

    Initial commit: add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+git init
+
+git add main.py
+git commit -m "Initial commit: add main.py"
+git remote add origin https://github.com/FukamiRui/python-study.git
+git branch -M main
+git push -u origin main
+
+
